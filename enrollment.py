b4e6f73a91a9bd2bf216595c37aae2b0197989f1
Sudarsan <70632244+sudarsan31@users.noreply.github.com>
2020-12-04 19:43:14 +0530

    Update enrollment.py

--- enrollment.py
+++ enrollment.py
@@ -3,7 +3,7 @@ import psycopg2,webbrowser
 import csv
 try:
       connection= psycopg2.connect(user = "postgres",
-                                  password = "sudarishu",
+                                  password = "********",
                                   host = "127.0.0.1",
                                   port = "5432");
       print("DataBase Opened Successfully")
